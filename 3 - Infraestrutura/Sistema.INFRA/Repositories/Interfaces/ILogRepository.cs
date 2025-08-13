81b4a29d11d3a36a3e4a7e51bf2930bd6397b293
RafaelPrumma <135171422+RafaelPrumma@users.noreply.github.com>
2025-08-13 17:09:43 -0300

    Remover interfaces duplicadas e ajustar EF

--- 3 - Infraestrutura/Sistema.INFRA/Repositories/Interfaces/ILogRepository.cs
+++
@@ -1,9 +0,0 @@
-using Sistema.CORE.Entities;
-
-namespace Sistema.CORE.Interfaces;
-
-public interface ILogRepository
-{
-    Task AdicionarAsync(Log log);
-    Task<IEnumerable<Log>> BuscarFiltradosAsync(DateTime? inicio, DateTime? fim, LogTipo? tipo);
-}
