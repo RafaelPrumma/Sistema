3b9b4c0f1beb70db1a5582af8e68149967b9f382
RafaelPrumma <135171422+RafaelPrumma@users.noreply.github.com>
2025-08-31 19:38:07 -0300

    Adiciona dashboard inicial com cards de estatisticas

--- 0 - Apresentacao/Sistema.MVC/Controllers/HomeController.cs
+++ 0 - Apresentacao/Sistema.MVC/Controllers/HomeController.cs
@@ -1,22 +1,62 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Sistema.MVC.Models;
+using Sistema.CORE.Interfaces;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Sistema.MVC.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
+        public class HomeController : Controller
+        {
+                private readonly ILogger<HomeController> _logger;
+                private readonly IUsuarioService _usuarioService;
+                private readonly IPerfilService _perfilService;
+                private readonly IFuncionalidadeService _funcionalidadeService;
+                private readonly IConfiguracaoService _configuracaoService;
+                private readonly IMensagemService _mensagemService;
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+                public HomeController(
+                        ILogger<HomeController> logger,
+                        IUsuarioService usuarioService,
+                        IPerfilService perfilService,
+                        IFuncionalidadeService funcionalidadeService,
+                        IConfiguracaoService configuracaoService,
+                        IMensagemService mensagemService)
+                {
+                        _logger = logger;
+                        _usuarioService = usuarioService;
+                        _perfilService = perfilService;
+                        _funcionalidadeService = funcionalidadeService;
+                        _configuracaoService = configuracaoService;
+                        _mensagemService = mensagemService;
+                }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+                public async Task<IActionResult> Index()
+                {
+                        var model = new DashboardViewModel();
+
+                        var usuarios = await _usuarioService.BuscarTodosAsync(1, 1);
+                        model.TotalUsuarios = usuarios.TotalCount;
+
+                        var perfis = await _perfilService.BuscarTodosAsync(1, 1);
+                        model.TotalPerfis = perfis.TotalCount;
+
+                        var funcs = await _funcionalidadeService.BuscarPaginadasAsync(1, 1);
+                        model.TotalFuncionalidades = funcs.TotalCount;
+
+                        var configs = await _configuracaoService.BuscarPorAgrupamentoAsync("AzureAd");
+                        model.TotalConfiguracoes = configs.Count();
+
+                        var userId = HttpContext.Session.GetInt32("UserId");
+                        if (userId is not null)
+                        {
+                                var msgs = await _mensagemService.BuscarCaixaEntradaAsync(userId.Value, 1, 1);
+                                model.TotalMensagens = msgs.TotalCount;
+                        }
+
+                        return View(model);
+                }
 
 		public IActionResult Privacy()
 		{
---
+++ 0 - Apresentacao/Sistema.MVC/Models/DashboardViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Sistema.MVC.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalUsuarios { get; set; }
+        public int TotalPerfis { get; set; }
+        public int TotalFuncionalidades { get; set; }
+        public int TotalConfiguracoes { get; set; }
+        public int TotalMensagens { get; set; }
+    }
+}
